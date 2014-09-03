734bb0ee25e9d0da23a27feef93791b3b57ae1b1
sofroni2 <sofroni2@f403994e-9d58-42ba-894d-10d84a558d0e>
2014-09-03 03:37:48 +0000

    submitting MP1

--- mp1/rgbapixel.h
+++ mp1/rgbapixel.h
@@ -13,7 +13,7 @@ class RGBAPixel{
 
 
 	RGBAPixel(); //constructor
-	RGBAPixel(uint8_t red, uint8_t blue, uint8_t green, uint8_t alpha);
+	RGBAPixel(uint8_t red, uint8_t blue, uint8_t green);
 
 
 	//members:
