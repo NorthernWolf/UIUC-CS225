80ca6c3684b6c8852ad036cab055f00b6411428b
sofroni2 <sofroni2@f403994e-9d58-42ba-894d-10d84a558d0e>
2014-09-03 01:56:13 +0000

    mp1 submission

--- mp1/rgbapixel.h
+++ mp1/rgbapixel.h
@@ -1,10 +1,10 @@
-#include <cstdint>
-#include <ostream>
 #ifndef RGBAPIXEL_H
 #define RGBAPIXEL_H
+#include <cstdint>
+#include <ostream>
 
 
-
+using std::uint8_t;
 
 
 class RGBAPixel{
@@ -13,7 +13,7 @@ class RGBAPixel{
 
 
 	RGBAPixel(); //constructor
-	RGBAPixel(uint8_t red, uint8_t blue, uint8_t green);
+	RGBAPixel(uint8_t newRed, uint8_t newBlue, uint8_t newGreen);
 
 
 	//members:
