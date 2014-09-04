d3ef2ab4f8c7d8bd7bc07e39119846e00f3eec28
sofroni2 <sofroni2@f403994e-9d58-42ba-894d-10d84a558d0e>
2014-09-04 19:37:36 +0000

    lab_debug submission

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -1,5 +1,9 @@
 #include <cstdlib>
+#include <iostream> //had to add for cout
 #include "png.h"
+
+
+using namespace std;//had to add for cout
 
 //
 // sets up the output image
@@ -13,11 +17,11 @@ PNG * setupOutput(int w, int h)
 // Returns my favorite color
 RGBAPixel * myFavoriteColor(int intensity)
 {
-	RGBAPixel color;
-	color.red   = 0;
-	color.green = intensity/2;
-	color.blue  = intensity;
-	return &color;
+	RGBAPixel *color= new RGBAPixel;//color used to be local but now we've de referenced it and make it a default object
+	color->red   = 0; //added pointer arrows
+	color->green = intensity/2;//added pointer arrow
+	color->blue  = intensity;//added pointer arrow
+	return color;//took off the &
 }
 
 
@@ -25,23 +29,25 @@ void sketchify()
 {
 	// Load in.png
 	PNG * original;
-
-	original->readFromFile("in.png");
+	cout << "Reached line 28" << endl;//added print statement
+	original = new PNG; //from lecture (9/3) we have to make a memory location for this object pointer
+	original -> readFromFile("in.png");
 	int width  = original->width();
 	int height = original->height();
-
+	cout << "Reached line 32" << endl;//added print statement
 	// Create out.png
-	PNG * output;
-	setupOutput(width, height);	
-
+	PNG * output = setupOutput(width, height);//we need to give the output file the width and height from original. 
+	//output = new PNG; //same as line 33
+	//setupOutput(width, height);	
+	
 	// Loud our favorite color to color the outline
 	RGBAPixel * myPixel = myFavoriteColor(192);
-
+	cout << "Reached line 44" << endl;//added print statement
 	// Go over the whole image, and if a pixel differs from that to its upper
 	// left, color it my favorite color in the output
-	for (int y = 1; 0 < y < height; y++)
+	for (int y = 1; y < height; y++) 
 	{
-		for (int x = 1; 0 < x < width; x++)
+		for (int x = 1;x < width; x++) 
 		{
 			// Calculate the pixel difference
 			RGBAPixel * prev = (*original)(x-1, y-1);
@@ -54,15 +60,18 @@ void sketchify()
 			// color the output pixel with my favorite color
 			RGBAPixel * currOutPixel = (*output)(x,y);
 			if (diff > 100)
-				currOutPixel = myPixel;
+				* currOutPixel = * myPixel; //we need to dereference!	
 		}
 	}
-	
+	cout << "Reached line 66" << endl;//added print statement	
 	// Save the output file
 	output->writeToFile("out.png");
-
+	cout << "Reached line 71" << endl;//added print statement	
 	// Clean up memory
-	delete myPixel;
+	delete myPixel; 
+	cout << "Reached line 74" << endl;//added print statement	
 	delete output;
+	cout << "Reached line 76" << endl;//added print statement	
 	delete original;
+	cout << "Reached line 78" << endl;//added print statement	
 }
